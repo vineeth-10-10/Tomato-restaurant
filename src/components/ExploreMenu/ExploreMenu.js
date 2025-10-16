10be905b9550a5dd1b84b69c4262974a1355cae4
vineeth-10-10 <vineethkalewar62@gmail.com>
2025-10-16 13:31:03 +0530

    completed ExploreMenu  Component with selecting seperate menu category

--- src/components/ExploreMenu/ExploreMenu.js
+++ src/components/ExploreMenu/ExploreMenu.js
@@ -1,12 +1,27 @@
-import './ExploreMenu.css';
-import { menu_list } from '../../assets/assets';
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div>
-      
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Great food, fast delivery, and zero hassle. Pick your favorites from our
+        chef’s special menu and let the feast come straight to your door!
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+            return(
+                  <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                      <img  className={category===item.menu_name?"active":""} src={item.menu_image} alt=""/>
+                      <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default ExploreMenu
+export default ExploreMenu;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,16 @@
 
 import Header from '../../components/Header/Header';
 import './Home.css';
-
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import { useState } from 'react';
 
 const Home = () => {
+  const[category,setCategory] = useState("All");
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div> 
   )
 }
